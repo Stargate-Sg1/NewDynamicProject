cad40871c1aa52abbc48af2f4cd33c284cf49e73
Stargate-Sg1 <s.cariani86@gmail.com>
2017-05-06 10:18:15 +0200

    aggiunta barra di navigazione e materialize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require turbolinks
+//= require materialize-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
